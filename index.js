2b5d383f23a1a242a3431125492594585149890e
App Academy Student <student@appacademy.io>
2014-04-02 10:50:08 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+require("./lib/util.js");
+require("./lib/moving_object.js");
+require("./lib/asteroid.js");
+require("./lib/bullet.js");
+require("./lib/ship.js");
+require("./lib/game.js");
+
+module.exports = global.Asteroids;
